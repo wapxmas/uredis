ff8b21811e954736aebb0e0b321f6701cce8cdee
Sergei Iashin <yashin.sergey@gmail.com>
2021-01-31 02:21:48 +0300

    fake failed test has been removed

--- tests/test.cpp
+++ tests/test.cpp
@@ -6,7 +6,6 @@ unsigned int Factorial( unsigned int number ) {
 }
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
-REQUIRE( Factorial(0) == 1 );
 REQUIRE( Factorial(1) == 1 );
 REQUIRE( Factorial(2) == 2 );
 REQUIRE( Factorial(3) == 6 );
